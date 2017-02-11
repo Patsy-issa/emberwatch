2c6dd89c07f470833a0232f9fb63e81d869d5afe
patsy-issa <patsy.issa@gmail.com>
2017-02-11 16:26:06 +0100

    wip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,16 @@
 //NB: These load in the order of first to last! Get the order right or face the consequences!!!!
 var enabled_modules = [
-	"segment",
-	"inline_videos"
+	'segment',
+	'inline_videos'
 ];
 
 enabled_modules.forEach(function(module){
+	var script = document.createElement('script');
 	
-	var script = document.createElement("script");
-	
-	script.id = "ew_" + module;
-	script.setAttribute("type", "text/javascript");
+	script.id = 'ew_' + module;
+	script.setAttribute('type', 'text/javascript');
 
-	script.src = "/javascripts/" + module + ".js";
+	script.src = document.baseURI + '/javascripts/' + module + '.js';
 
 	document.body.appendChild(script);
-
 });
